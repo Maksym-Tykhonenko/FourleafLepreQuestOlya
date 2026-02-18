618962299ff7a9d6845516fd2a232d69f5b099cc
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-02-18 15:40:49 +0200

    fix exp date

--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -337,7 +337,7 @@ export default function RootNavigator() {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2026-02-10T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2026-02-20T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
